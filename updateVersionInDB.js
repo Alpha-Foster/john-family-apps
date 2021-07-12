66d21a2e333b6829bbb654088772db9dd554e158
Jeffrin Manova Prabahar <JeffrinManovaPrabahar@gmail.com>
2021-07-12 23:20:36 +0530

    Added script to update version in db.

---
+++ updateVersionInDB.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
+const URL = process.env.DB_CONNECTION_STRING || ''
+const VERSION = process.env.ANDROID_APP_VERSION || ''
+
+MongoClient.connect(URL, function (err, db) {
+    if (err) throw err;
+    const dbo = db.db("grocery");
+    dbo.collection("appDetails").updateOne({}, {
+        $set: {
+            latestAndroidVersion: VERSION
+        }
+    }, function (err, result) {
+        if (err) throw err;
+        console.log("Android version updated in DB.");
+        db.close();
+    });
+});
